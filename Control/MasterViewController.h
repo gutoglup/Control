9ca79057a7988a7a7c273e420bc3a667b4797cfd
Augusto Reis <reis-augusto@hotmail.com>
2014-07-28 14:35:42 -0300

    Initial Commit

---
+++ Control/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Control
+//
+//  Created by Augusto Reis on 28/07/14.
+//  Copyright (c) 2014 Augusto Reis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
